29ac7bd2a65587300304700a1fae78927c15ed4f
Daniel Tan <dtch1997@users.noreply.github.com>
2025-02-01 17:31:54 -0800

    update

--- src/steg_games/model_api/wrapper.py
+++ src/steg_games/model_api/wrapper.py
@@ -8,18 +8,19 @@ from steg_games.core._types import Message
 class RateLimiter(ModelAPIInterface):
     """Rate limits requests to the underlying model API."""
     
-    def __init__(self, api: ModelAPIInterface, max_requests_per_minute: int):
+    def __init__(self, api: ModelAPIInterface, max_requests_per_period: int, period_length: float):
         """
         Initialize the rate limiter.
         
         Args:
             api: The underlying ModelAPI implementation
-            max_requests_per_minute: Maximum number of requests allowed per minute
+            max_requests_per_period: Maximum number of requests allowed per period
+            period_length: Length of the rate limiting period (in seconds)
         """
         self.api = api
-        self.max_requests_per_minute = max_requests_per_minute
+        self.max_requests_per_period = max_requests_per_period
+        self.period_length = period_length
         self.request_times = deque()
-        self.window_size = 60  # 1 minute in seconds
 
     async def _wait_if_needed(self):
         """
@@ -29,12 +30,12 @@ class RateLimiter(ModelAPIInterface):
         current_time = time.time()
         
         # Remove timestamps older than our window
-        while self.request_times and current_time - self.request_times[0] >= self.window_size:
+        while self.request_times and current_time - self.request_times[0] >= self.period_length:
             self.request_times.popleft()
         
         # If we've hit our limit, wait until we can make another request
-        if len(self.request_times) >= self.max_requests_per_minute:
-            wait_time = self.request_times[0] + self.window_size - current_time
+        if len(self.request_times) >= self.max_requests_per_period:
+            wait_time = self.request_times[0] + self.period_length - current_time
             if wait_time > 0:
                 await asyncio.sleep(wait_time)
         
